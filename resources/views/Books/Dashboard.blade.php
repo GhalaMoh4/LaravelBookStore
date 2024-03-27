bf9362df2830c41f2f80d7eac6b8d8f975d20006
ghala <vvip36281@gmail.com>
2024-03-27 09:42:40 +0300

    first commit

---
+++ resources/views/Books/Dashboard.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Grafik</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+
+<body>
+    @include('/Books/Navbar')
+
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col">
+                <h3>Sale</h3>
+                <canvas id="salesChart" width="400" height="200"></canvas>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+        $(document).ready(function() {
+            var ctx = document.getElementById('salesChart').getContext('2d');
+            var salesChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ['Harry Potter', 'The Old Man and the Sea', 'Pride and Prejudice', 'To Have and Have Not', 'Julius Caesar', 'Romeo and Juliet	'],
+                    datasets: [{
+                        label: 'Sale',
+                        data: [100, 120, 90, 125, 50, 175],
+                        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                        borderColor: 'rgba(54, 162, 235, 1)',
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+        });
+    </script>
+</body>
+
+</html>
